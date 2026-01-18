49147fad8c7086681593ac86fbb0f416ae0b2911
psjw <dlrudtn108@gmail.com>
2026-01-18 21:10:43 +0900

    init project

---
+++ src/main/java/com/psjw/kafkahandson/api/MyController.java
@@ -0,0 +1,13 @@
+package com.psjw.kafkahandson.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/hello")
+    String hello() {
+        return "Hello World";
+    }
+}
